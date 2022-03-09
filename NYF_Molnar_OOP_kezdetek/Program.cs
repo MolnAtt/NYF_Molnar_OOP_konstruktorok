2cb778f25bb9712f0b00f353659e9916477866c3
Molnár Attila <molnar.attila@szlgbp.hu>
2022-03-09 16:48:14 +0100

    Add project files.

---
+++ NYF_Molnar_OOP_kezdetek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYF_Molnar_OOP_kezdetek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
